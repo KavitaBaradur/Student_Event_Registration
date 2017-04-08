411acd265e153bed1b15515bbcdb34d760366676
Guanxiong Liu <sylorbeijing@gmail.com>
2017-04-08 12:10:20 -0400

    refine organizer feedback view

--- app/src/main/java/com/team2_wpi/android/student_event_registration/OrganizerFeedbackActivity.java
+++ app/src/main/java/com/team2_wpi/android/student_event_registration/OrganizerFeedbackActivity.java
@@ -1,11 +1,17 @@
 package com.team2_wpi.android.student_event_registration;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import com.team2_wpi.android.student_event_registration.data.SQLCommand;
+import com.team2_wpi.android.student_event_registration.util.DBOperator;
 
 /**
  * Created by sylor on 4/7/17.
@@ -13,6 +19,8 @@ import android.widget.Button;
 
 public class OrganizerFeedbackActivity extends AppCompatActivity implements View.OnClickListener {
     private Button back;
+    private String org_id;
+    private ListView listView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,8 +29,27 @@ public class OrganizerFeedbackActivity extends AppCompatActivity implements View
         setContentView(R.layout.organizer_feedback);
         // find elements
         back = (Button) findViewById(R.id.org_feedback_back);
+        listView = (ListView) findViewById(R.id.org_feedback_lv);
         // set up on click
         back.setOnClickListener(this);
+        // visualize feedbacks
+        visualFeedback();
+    }
+
+    private void visualFeedback() {
+        // init args
+        String init_args[] = new String[1];
+        init_args[0] = org_id;
+        // execute the sql
+        Cursor cursor = DBOperator.getInstance().execQuery(SQLCommand.ORG_FEEDBACK, init_args);
+        // bind the data to list
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getApplicationContext(),
+                                                              R.layout.org_event_listitem,
+                                                              cursor,
+                                                              new String[] { "name", "great", "fair", "need_imp" },
+                                                              new int[] { R.id.org_feedback_name, R.id.org_feedback_great, R.id.org_feedback_fair, R.id.org_feedback_need_imp },
+                                                              SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE);
+        listView.setAdapter(adapter);
     }
 
     @Override
--- app/src/main/java/com/team2_wpi/android/student_event_registration/data/SQLCommand.java
+++ app/src/main/java/com/team2_wpi/android/student_event_registration/data/SQLCommand.java
@@ -14,5 +14,10 @@ public abstract class SQLCommand {
     public static String ORG_EVENT_HIS = "select Event_ID as _id, Event_Name as name, Event_Type as type, Event_Date as date " +
                                          "from Event " +
                                          "where Org_ID = ?";
-
+    // init org feedback history
+    public static String ORG_FEEDBACK = "select E.Event_ID as _id, E.Event_Name as name, SUM(F.Great) as great, SUM(F.Fair) as fair, SUM(F.Need_Imp) as need_imp " +
+                                         "from Event E, Feedback F " +
+                                         "where E.Event_ID = F.Event_ID " +
+                                         "and E.Org_ID = ? " +
+                                         "group by E.Event_ID";
 }

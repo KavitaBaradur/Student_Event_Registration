e52ecde1971bbc6a95a55af2a52f40dafbf911c3
Kavita <kavitabaradur@gmail.com>
2017-04-11 18:01:54 -0400

    NavigationView

--- app/src/main/java/com/team2_wpi/android/student_event_registration/data/SQLCommand.java
+++ app/src/main/java/com/team2_wpi/android/student_event_registration/data/SQLCommand.java
@@ -6,8 +6,14 @@ package com.team2_wpi.android.student_event_registration.data;
 
 public abstract class SQLCommand {
 
-    public static String QUERY_1 = "select STD_PASSWORD from STUDENT where STD_NAME=?";
+    public static String QUERY_1 = "select STD_PASSWORD from STUDENT where STD_ID=?";
 
     public static String QUERY_2 = "select * from Event";
+
+    public static String QUERY_3 = "select STD_NAME from Student where STD_ID=?";
+
+    public static String QUERY_4 = "select STD_MAJOR from Student where STD_ID=?";
+
+    public static String QUERY_5 = "update STUDENT set STD_PASSWORD=? where STD_ID=?";
 
 }
